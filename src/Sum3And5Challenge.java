e18f224c69019bad608e41395f90bf5cc8b70038
Patryk Gadziński <pgadzinski@cross-net.eu>
2025-07-16 12:47:28 +0200

    fix operators

--- src/Sum3And5Challenge.java
+++ src/Sum3And5Challenge.java
@@ -5,7 +5,7 @@ public class Sum3And5Challenge {
 
         for (int i = 1; i <= 1000; i++) {
             if ((i % 3 == 0) && (i % 5 == 0)) {
-                sum = sum + i;
+                sum += i;
                 System.out.println(i + " is being added");
                 count++;
                 if (count >= 5) {
@@ -13,6 +13,6 @@ public class Sum3And5Challenge {
                 }
             }
         }
-        System.out.println("The added numbers sum up to: " + sum);
+        System.out.println(count + " numbers divisible by 3 and 5 add up to: " + sum);
     }
 }
